3121ec9b806eaacb9cf75c59e5915dc7fb948c0b
Mohamed Shimy <msh.work@outlook.sa>
2023-05-04 12:15:30 +0300

    Initial Commit

---
+++ StretchyHeader/StretchyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchyHeaderApp.swift
+//  StretchyHeader
+//
+//  Created by Mohamed Shemy on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct StretchyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
